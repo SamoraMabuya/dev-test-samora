35bd94e7b563eb17330ef6fc2b63dbf355c7638d
Samora Mabuya <samora@kriterion.ai>
2025-02-11 13:21:44 +0200

    Unused hello.ts and title.tsx deleted

--- src/pages/vehicle/[id]/title.tsx
+++
@@ -1,5 +0,0 @@
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { GetServerSideProps } from "next";
-import Head from "next/head";
